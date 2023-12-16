877e91590729387012b11fb29480943844284f51
Ernesto Boca <boca.ernesto13@gmail.com>
2023-12-16 06:34:24 -0800

    ass

--- add.c
+++ add.c
@@ -1,12 +1,32 @@
 #include "monty.h"
 /**
- * f_add -  adds the top two elements of the stack.
- * @head: the top of the stackk
+ * f_add - swaps the top two elements of the stack.
+ * @head: the top of the stack
  * @line_number: the line number
  * Return: nothing
  */
 void f_add(stack_t **head, unsigned int line_number)
 {
-	(void)head;
-	(void)line_number;
+	stack_t *temp;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fclose(bus.fp);
+		_free(head);
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->prev == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fclose(bus.fp);
+		_free(head);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *head;
+	*head = (*head)->prev;
+	(*head)->n += temp->n;
+	(*head)->next = NULL;
+	free(temp);
 }
